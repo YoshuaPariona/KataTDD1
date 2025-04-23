f39c8d2f8c7a808984f040bc71868865f302fe85
YoshuaPariona <loganpariona.1207@gmail.com>
2025-04-23 11:26:56 -0500

    Caso 2: Calcular promedio con un conjunto de un elemento (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -6,7 +6,6 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
-
-    def promedio(self):
-        return False
-
+    def test_conjunto_unElemento_retornaValorUnicoElemento( self ):
+        conjunto=Conjunto([5])
+        self.assertEqual(5,conjunto.promedio())
